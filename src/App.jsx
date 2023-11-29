0273e5c70a484f1e93a5e2cbd7b7809055a6c685
llorens4 <mathys.llorens@etu.unilim.fr>
2023-11-29 10:16:03 +0100

    t

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Head from '/Head.jsx'
 export default function App() {
   return (
     <>
       <header>
-
+        <Head />
       </header>
       <main className="h-min-screen bg-star-pattern">
       </main>
