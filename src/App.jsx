3fc016299f55753ecb9d027ae970c571d7c3b25e
llorens4 <mathys.llorens@etu.unilim.fr>
2023-11-29 10:50:04 +0100

    tt

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import Head from './components/Head'
+import Head from "./components/Head";
 export default function App() {
   return (
     <>
-      <header className=''>
+      <header className="">
         <Head />
       </header>
-      <main className="h-min-screen bg-star-pattern">
+      <main className="h-min-screen bg-mo bg-star-pattern">
+        <p>hello</p>
       </main>
       <footer>
-
+        <p> hello</p>
       </footer>
     </>
   );
