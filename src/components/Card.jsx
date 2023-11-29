3c98016e4fe68b9948a4f3a54c8e2f5c7c31b026
llorens4 <mathys.llorens@etu.unilim.fr>
2023-11-29 11:25:03 +0100

    V22

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ export default function Card({ data }) {
   const { image,summary,id,name} = data;
   return (
 
-      <div className=" w-44 p-2 h-auto bg-slate-700" key={name}>
+      <div className=" w-44 p-2 h-screen" key={name}>
         <img src={image.medium} alt={name} />
         <h1>{summary}</h1>
       </div>
