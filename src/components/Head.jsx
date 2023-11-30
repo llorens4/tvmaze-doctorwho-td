59e2f6c8782bb897f416ca6e1feb99bea271e872
llorens4 <mathys.llorens@etu.unilim.fr>
2023-11-30 10:26:46 +0100

    tt

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -10,19 +10,19 @@ export default function Head() {
         className="max-w-[15rem] ml-2"
       />
       <div className="text-2xl">
-        <button className="m-4 transition-all duration-200 text-white hover:text-gold border-b-gold">
+        <button className="m-4 transition-all duration-500 text-white hover:text-gold border-b-gold">
           Accueil
         </button>
-        <button className="m-4 transition-all duration-200 text-white hover:text-gold border-b-gold">
+        <button className="m-4 transition-all duration-500 text-white hover:text-gold border-b-gold">
           Episode
         </button>
-        <button className="m-4 transition-all duration-200 text-white hover:text-gold border-b-gold">
+        <button className="m-4 transition-all duration-500 text-white hover:text-gold border-b-gold">
           Cast
         </button>
-        <button className="m-4 transition-all duration-200 text-white hover:text-gold border-b-gold">
+        <button className="m-4 transition-all duration-500 text-white hover:text-gold border-b-gold">
           Gallery
         </button>
-        <button className="m-4 transition-all duration-200 text-white hover:text-gold border-b-gold">
+        <button className="m-4 transition-all duration-500 text-white hover:text-gold border-b-gold">
           News
         </button>
       </div>
