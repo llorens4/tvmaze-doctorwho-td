3f7f42128e6ce2b852cfe4621fcafd16805abe27
hanxiaoyang18 <xiaoyang.han@etu.unilim.fr>
2023-11-30 11:17:19 +0100

    tt

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,28 +1,41 @@
 import EpisodeCard from "./EpisodeCard";
 
-export default function Cards({ data }) {
-  const dataSaison = data.filter((episode) => episode.season == 1);
+import { useState } from "react";
+
+export default function Cards({ seasons, episodes }) {
+ 
+  
+  // seasons est un tableau contenant toutes les saisons
+  // episodes est un tableau contenant tous les épisodes
+
+  // La saison séléctionnée
+  const [saison, setSaison] = useState(1)
+
+
+  // episodes est un tableau contenant tous les épisodes d'une saison
+  const episodesSaison = episodes.filter((episode) => episode.season == saison);
+
+
+  // Pour changer de saison
+  const handlerClick = (e) => {
+    e.preventDefault()
+    setSaison(e.target.dataset.saison)
+  }
+
+
   return (
     <>
       <div className="">
         <h1 className=" ml-10 text-2xl font-semibold text-white ">Episode</h1>
+
         <ul className=" ml-10 mt-10 text-lg font-medium text-white grid-cols-13 grid-flow-col grid grid-rows-1 ">
-          <li>S1</li>
-          <li>S2</li>
-          <li>S3</li>
-          <li>S4</li>
-          <li>S5</li>
-          <li>S6</li>
-          <li>S7</li>
-          <li>S8</li>
-          <li>S9</li>
-          <li>S10</li>
-          <li>S11</li>
-          <li>S12</li>
-          <li>S13</li>
+          {seasons.map((saison,i)=> <li key = {saison.id}><button data-saison = {i+1} onClick={handlerClick} >{saison.number}</button></li>)}
         </ul>
+
+   
+
         <div className="flex flex-wrap">
-          {dataSaison.map((episode) => (
+          {episodesSaison.map((episode) => (
             <EpisodeCard data={episode} id={episode.id} />
           ))}
         </div>
