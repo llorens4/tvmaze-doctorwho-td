346febe6de08484c90fb827f983be3d714d400a1
llorens4 <mathys.llorens@etu.unilim.fr>
2023-11-30 15:27:47 +0100

    tt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,12 @@ export default function Home() {
         <img
           src="https://static.tvmaze.com/uploads/images/original_untouched/486/1217372.jpg"
           alt="logo doctor who"
-          className="justify-center max-w-6xl"
+          className="justify-center px-6 py-6 aspect-auto"
         />
-        <h1 className="font-CCTimelord">Doctor Who</h1>
+        <h1 className="font-CCtimelord text-6xl pt-12">Doctor Who</h1>
 
         <div>
-          <h1 className="">
+          <h1 className="flex text-xl">
             The Doctor and friends travel from the dawn of human history to
             distant alien worlds. And everywhere they go, they find adventure,
             terror, fun, chases, joy and monsters.
