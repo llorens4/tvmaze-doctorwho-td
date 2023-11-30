02b8f7c7ca315c3a4631f64a0e3b9cc220927c8e
hanxiaoyang18 <xiaoyang.han@etu.unilim.fr>
2023-11-30 09:03:02 +0100

    tt

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,7 +6,22 @@ export default function Cards({ data }) {
     <>
       <div className="">
         <h1 className=" ml-10 text-2xl font-semibold text-white ">Episode</h1>
-        <div className="grid-cols-4 grid-flow-col grid grid-rows-2">
+        <ul className=" ml-10 mt-10 text-lg font-medium text-white grid-cols-13 grid-flow-col grid grid-rows-1 ">
+          <li>S1</li>
+          <li>S2</li>
+          <li>S3</li>
+          <li>S4</li>
+          <li>S5</li>
+          <li>S6</li>
+          <li>S7</li>
+          <li>S8</li>
+          <li>S9</li>
+          <li>S10</li>
+          <li>S11</li>
+          <li>S12</li>
+          <li>S13</li>
+        </ul>
+        <div className="grid-cols-6 grid-flow-col grid grid-rows-2">
           {dataSaison.map((episode) => (
             <EpisodeCard data={episode} id={episode.id} />
           ))}
