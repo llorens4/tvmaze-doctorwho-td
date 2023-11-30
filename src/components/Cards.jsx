99bda78def38fc09205fd3b0ae81958712d2a659
hanxiaoyang18 <xiaoyang.han@etu.unilim.fr>
2023-11-30 08:39:35 +0100

    tt

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,12 +1,16 @@
 import EpisodeCard from "./EpisodeCard";
 
 export default function Cards({ data }) {
-  const dataSaison = data.filter((episode) =>  episode.season==1)
+  const dataSaison = data.filter((episode) => episode.season == 1);
   return (
     <>
       <div className="">
-        {dataSaison.map(episode => <EpisodeCard data={episode} id={episode.id} />)}
-
+        <h1 className=" ml-10 text-2xl font-semibold text-white ">Episode</h1>
+        <div className="grid-cols-4 grid-flow-col grid grid-rows-2">
+          {dataSaison.map((episode) => (
+            <EpisodeCard data={episode} id={episode.id} />
+          ))}
+        </div>
       </div>
     </>
   );
--- src/components/EpisodeCard.jsx
+++ src/components/EpisodeCard.jsx
@@ -2,10 +2,10 @@ export default function EpisodeCard({ data }) {
   const { image,summary,id,name} = data;
   return (
 
-      <div className=" w-44 p-2 h-auto bg-vortex-texture m-10 text-red-50" key={name}>
+      <div className=" w-44 p-3 h-auto bg-vortex-texture m-10 text-red-50" key={name}>
         <img src={image.medium} alt={name} />
-        <h1 className="mt-2 text-lg font-semibold">{name}</h1>
-        <h1 className="mt-2  text-sm mb-2">{summary}</h1>
+        <h1 className="mt-3 text-lg font-semibold">{name}</h1>
+        <h1 className="mt-2 text-sm mb-2">{summary}</h1>
       </div>
 
   );
